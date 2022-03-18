e5014e61960dcb1bbb70dc9803585c8702a7c0e2
sriyabetageri <75461862+sriyabetageri@users.noreply.github.com>
2022-03-17 18:58:29 -0700

    Add files via upload

---
+++ screens/Details.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from "react";
+import {View, Text, FlatList, StyleSheet, Alert, SafeAreaView} from "react-native";
+
+export default class DetailsScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text> Details Screen </Text>
+            </View>
+        )
+    }
+}
